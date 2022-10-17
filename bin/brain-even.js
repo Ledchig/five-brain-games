4c81bf49636be8c7f6783058cdd03d952d60f026
Nikolay Savonin <s9826059221@gmail.com>
2022-10-17 16:24:18 +0500

    update brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -7,29 +7,39 @@ console.log(`Hello, ${name}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 // Проверка на четность.
-const evenNumber = (number) => number % 2 === 0;
-let randomNum = 0;
+const checkEvenNumber = (number) => number % 2 === 0;
+const letRandomNum = () => Math.round(Math.random() * 100);
 let sumCorrectAnswer = 0;
-// Логика игры на четность.
-const brainEven = () => {
-  for (let i = 0; i < 3; i += 1) {
-  // получение случайного числа.
-    randomNum = Math.round(Math.random() * 100);
-    // вопрос и ввод игрока.
-    console.log(`Question: ${randomNum}`);
+// Генерируем три вопроса.
+const quest1 = letRandomNum();
+const quest2 = letRandomNum();
+const quest3 = letRandomNum();
+const correctAnswer1 = checkEvenNumber(quest1) ? 'yes' : 'no';
+const correctAnswer2 = checkEvenNumber(quest2) ? 'yes' : 'no';
+const correctAnswer3 = checkEvenNumber(quest3) ? 'yes' : 'no';
+// Создаем массив с вопросами и верными ответами.
+const arrayWithQuests = [[quest1, correctAnswer1], [quest2, correctAnswer2],
+  [quest3, correctAnswer3]];
+// Сравнение ответов.
+const askQuestAndCheckAnswer = (questsAndCorrectAnswers) => {
+  for (let i = 0; i < questsAndCorrectAnswers.length; i += 1) {
+    console.log(`Question: ${questsAndCorrectAnswers[i][0]}`);
     const answer = readlineSync.question('Your answer:');
-    const correctAnswer = evenNumber(randomNum) ? 'yes' : 'no';
-    // проверка ответа.
-    if (answer === correctAnswer) {
+    if (answer === questsAndCorrectAnswers[i][1]) {
       console.log('Correct!');
       sumCorrectAnswer += 1;
     } else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${questsAndCorrectAnswers[i][1]}.`);
       break;
     }
   }
-  // проверяем счетчик верных ответов и поздравляем, либо предлогаем попытаться снова.
-  return sumCorrectAnswer === 3 ? console.log(`Congratulations, ${name}!`) : console.log(`Let's try again, ${name}`);
+  if (sumCorrectAnswer === 3) {
+    console.log(`Congratulations, ${name}!`);
+  } else {
+    console.log(`Let's try again, ${name}`);
+  }
 };
-brainEven();
-export default brainEven;
+
+askQuestAndCheckAnswer(arrayWithQuests);
+
+export default askQuestAndCheckAnswer;
